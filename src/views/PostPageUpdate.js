fcfea01a1f00a3318ab85e90bdffc4c7839a45ff
selene5692 <selenium5692@gmail.com>
2024-06-04 14:23:37 +0800

    added image update function

--- src/views/PostPageUpdate.js
+++ src/views/PostPageUpdate.js
@@ -1,30 +1,39 @@
 import React, { useEffect, useState } from "react";
-import { Button, Container, Form, Nav, Navbar } from "react-bootstrap";
+import { Button, Container, Form, Image, Nav, Navbar } from "react-bootstrap";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate, useParams } from "react-router-dom";
-import { auth, db } from "../firebase";
+import { auth, db, storage } from "../firebase";
 import { doc, getDoc, updateDoc } from "firebase/firestore";
 import { signOut } from "firebase/auth";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 
 export default function PostPageUpdate() {
   const params = useParams();
   const id = params.id;
   const [caption, setCaption] = useState("");
   const [image, setImage] = useState("");
+  const [previewImage, setPreviewImage] = useState(
+    "https://zca.sg/img/placeholder"
+  );
   const [user, loading] = useAuthState(auth);
   const navigate = useNavigate();
 
 
   async function updatePost() {
-    await updateDoc(doc(db, "posts", id), { caption, image });
+    const imageReference = ref(storage, `images/${image.name}`);
+    const response = await uploadBytes(imageReference, image);
+    const imageUrl = await getDownloadURL(response.ref);
+    await updateDoc(doc(db, "posts", id), { caption, image: imageUrl });
     navigate("/");
   }
+
 
   async function getPost(id) {
     const postDocument = await getDoc(doc(db, "posts", id));
     const post = postDocument.data();
     setCaption(post.caption);
     setImage(post.image);
+    setPreviewImage(post.image);
   }
 
   useEffect(() => {
@@ -57,19 +66,31 @@ export default function PostPageUpdate() {
               onChange={(text) => setCaption(text.target.value)}
             />
           </Form.Group>
-
+          <Image
+            src={previewImage}
+            style={{
+              objectFit: "cover",
+              width: "10rem",
+              height: "10rem",
+            }}
+          />
+          
           <Form.Group className="mb-3" controlId="image">
-            <Form.Label>Image URL</Form.Label>
+            <Form.Label>Image</Form.Label>
             <Form.Control
-              type="text"
-              placeholder="https://zca.sg/img/1"
-              value={image}
-              onChange={(text) => setImage(text.target.value)}
+              type="file"
+              onChange={(e) => {
+                const imageFile = e.target.files[0];
+                const previewImage = URL.createObjectURL(imageFile);
+                setImage(imageFile);
+                setPreviewImage(previewImage);
+              }}
             />
             <Form.Text className="text-muted">
-              Make sure the url has a image type at the end: jpg, jpeg, png.
+              Make sure the file has a image type at the end: jpg, jpeg, png.
             </Form.Text>
           </Form.Group>
+
           <Button variant="primary" onClick={(e) => updatePost()}>
             Submit
           </Button>
