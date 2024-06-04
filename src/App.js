9d2db771bfede0535246467ad0f70c22eb258c67
selene5692 <selenium5692@gmail.com>
2024-06-04 10:42:42 +0800

    updated add, update and post details; added db functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "./views/LoginPage";
+import PostPageAdd from "./views/PostPageAdd";
+import PostPageDetails from "./views/PostPageDetails";
 import PostPageHome from "./views/PostPageHome";
+import PostPageUpdate from "./views/PostPageUpdate";
 import SignUpPage from "./views/SignUpPage";
+
 
 function App() {
   return (
@@ -10,6 +14,9 @@ function App() {
         <Route path="/" element={<PostPageHome />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/add" element={<PostPageAdd />} />
+        <Route path="/post/:id" element={<PostPageDetails />} />
+        <Route path="/update/:id" element={<PostPageUpdate />} />
       </Routes>
     </BrowserRouter>
   );
